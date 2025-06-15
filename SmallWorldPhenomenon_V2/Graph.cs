65ee56c8c738b5778893d99e43803305b204abf0
OsamaMansour0110 <your-email@domain.com>
2025-06-15 03:08:55 +0300

    Initial upload of Small World Phenomenon project

---
+++ SmallWorldPhenomenon_V2/Graph.cs
@@ -0,0 +1,370 @@
+﻿namespace SmallWorldPhenomenon_V2
+{
+    static class Graph
+    {
+        private static Dictionary<int, List<string>> movieMap = new Dictionary<int, List<string>>();
+        private static Dictionary<int, HashSet<int>> actorMap = new Dictionary<int, HashSet<int>>();
+
+        private static Dictionary<string, int> actorEncoding = new Dictionary<string, int>();
+        private static Dictionary<int, string> numberEncoding = new Dictionary<int, string>();
+        private static int counter = 0;
+
+        public static void Initialize(string actor, string movie)
+        {
+            if (!actorEncoding.ContainsKey(actor))
+            {
+                actorEncoding.Add(actor, counter);
+                numberEncoding.Add(counter++, actor);
+
+                if (!movieMap.ContainsKey(actorEncoding[actor]))
+                {
+                    movieMap.Add(actorEncoding[actor], new List<string>());
+                    actorMap.Add(actorEncoding[actor], new HashSet<int>());
+                }
+            }
+
+            movieMap[actorEncoding[actor]].Add(movie);
+        }
+
+        public static void AddAdjacent(string actor, string otherActor)
+        {
+            actorMap[actorEncoding[actor]].Add(actorEncoding[otherActor]);
+            actorMap[actorEncoding[otherActor]].Add(actorEncoding[actor]);
+        }
+
+        public static void ParseQuery(string src, string dst)
+        {
+            Console.WriteLine($"{src}/{dst}");
+
+            Dictionary<int, int> level = GetDegreeOfSeparation(src, dst);
+
+            Console.Write($"DoS = {level.Last().Value}");
+            List<List<int>> paths = GetAllPaths(level);
+
+            List<List<string>> chainsOfMovies = GetRelationStrength(paths);
+
+            List<string> shortestChain = FindShortestChainOfMovies(chainsOfMovies);
+            PrintShortestChainOfMovies(shortestChain);
+           //PrintAllPaths(paths);
+
+        }
+
+        private static Dictionary<int, int> GetDegreeOfSeparation(string src, string dst)
+        {
+            int srcInt = actorEncoding[src];
+            int dstInt = actorEncoding[dst];
+
+            Queue<int> nodes = new Queue<int>();
+            bool[] visited = new bool[actorMap.Count];
+
+            nodes.Enqueue(srcInt);
+            visited[srcInt] = true;
+
+            Dictionary<int, int> level = new Dictionary<int, int>();
+            level.Add(srcInt, 0);
+
+            while (nodes.Count != 0)
+            {
+                int node = nodes.Dequeue();
+                
+                foreach (int actor in actorMap[node])
+                {
+                    if (!visited[actor])
+                    {
+                        visited[actor] = true;
+                        level.Add(actor, level[node] + 1);
+
+                        if (actor == dstInt)
+                        {
+                            return level;
+                        }
+
+                        nodes.Enqueue(actor);
+                    }
+                }
+            }
+
+            return level;
+        }
+
+        private static List<int> GetPath(Dictionary<int, int> level)
+        {
+            List<int> path = new List<int>();
+
+            path.Add(level.Last().Key);
+            int index = 0;
+            int currentLevel = level.Last().Value;
+
+            foreach (KeyValuePair<int, int> pair in level.Reverse())
+            {
+                if (actorMap[pair.Key].Contains(path[index]))
+                {
+                    if (pair.Value == currentLevel - 1)
+                    {
+                        path.Add(pair.Key);
+                        currentLevel = pair.Value;
+                        index++;
+                        continue;
+                    }
+
+                    else if (pair.Value == currentLevel && index != 0)
+                    {
+                        if (actorMap[pair.Key].Contains(path[index - 1]))
+                        {
+                            foreach (int p in path)
+                            {
+                                if (level.ContainsKey(p) && level[p] == pair.Value)
+                                {
+                                    level.Remove(p);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            path.Reverse();
+
+            return path;
+        }
+
+        private static List<List<int>> GetAllPaths(Dictionary<int, int> level)
+        {
+            //GetPath is called multiple times until it runs out of unique paths
+
+            List<List<int>> paths = new List<List<int>>();
+
+            List<int> firstPath = GetPath(level);
+            paths.Add(firstPath);
+
+            List<int> newPath = GetPath(level);
+            while (!firstPath.SequenceEqual(newPath))
+            {
+                paths.Add(newPath);
+                firstPath = newPath;
+                newPath = GetPath(level);
+            }
+
+            return paths;
+        }
+
+        private static List<List<string>> GetRelationStrength(List<List<int>> paths)
+        {
+            int[] strengths = new int[paths.Count];
+
+            List<List<string>> chains = new List<List<string>>();
+            int count = 0;
+
+            foreach (List<int> path in paths)
+            {
+                int total = 0;
+                chains.Insert(count, new List<string>());
+
+                for (int i = 0; i < path.Count - 1; i++)
+                {
+                    int actor = path[i];
+                    int otherActor = path[i + 1];
+
+                    List<string> movieList = movieMap[actor];
+                    List<string> otherMovieList = movieMap[otherActor];
+
+                    IEnumerable<string> commonMovies = movieList.Intersect(otherMovieList);
+                    foreach (string movie in commonMovies)
+                    {
+                        chains[count].Add(movie);
+                    }
+                    
+                    chains[count].Add("=>");
+                    total += commonMovies.Count();
+                }   
+                strengths[count] = total;
+                count++;
+            }
+
+            int str = strengths.Max();
+            Console.WriteLine($", RS = {str}");
+
+            int index = Array.IndexOf(strengths, str);
+            Console.Write("CHAIN OF ACTORS: ");
+
+            int j = 0;
+            while (j < paths[index].Count)
+            {
+                Console.Write(numberEncoding[paths[index][j]]);
+                while (j + 1 < paths[index].Count)
+                {
+                    Console.Write($" -> {numberEncoding[paths[index][j + 1]]}");
+                    j++;
+                }
+                j++;
+            }
+            Console.WriteLine();
+
+            return chains;
+        }
+
+        private static List<string> FindShortestChainOfMovies(List<List<string>> chains)
+        {
+            // Shortest chain of movies is defined by two attributes
+            // 1) The chain with the shallowest depth
+            // 2) The chains with the most movies
+
+            // If two chains are found to have the same depth, then the chain with the most movies
+            // is selected
+
+            int minLevel = -1;
+            int maxMovies = -1;
+            int chainIndex = -1;
+            for (int j = 0; j < chains.Count; j++)
+            {
+                int levelCount = 0;
+                int movieCount = 0;
+
+                foreach (string c in chains[j])
+                {
+                    if (c.Equals("=>"))
+                    {
+                        levelCount++;
+                    }
+                    else
+                    {
+                        movieCount++;
+                    }
+                }
+
+                if (minLevel == -1 || levelCount < minLevel)
+                {
+                    minLevel = levelCount;
+                    chainIndex = j;
+                }
+                else if (levelCount == minLevel && movieCount > maxMovies)
+                {
+                    chainIndex = j;
+                }
+
+                if (maxMovies == -1 || movieCount > maxMovies)
+                {
+                    maxMovies = movieCount;
+                }
+            }
+
+            List<string> shortestChain = chains[chainIndex];
+
+            return shortestChain;
+        }
+
+        private static void PrintShortestChainOfMovies(List<string> shortestChain)
+        {
+            Console.Write("CHAIN OF MOVIES: ");
+
+            int i = 0;
+            while (i < shortestChain.Count)
+            {
+                if (!shortestChain[i].Equals("=>"))
+                {
+                    Console.Write(shortestChain[i]);
+                }
+
+                while (i + 1 < shortestChain.Count)
+                {
+                    if (shortestChain[i + 1].Equals("=>"))
+                    {
+                        if (i + 2 < shortestChain.Count)
+                        {
+                            Console.Write(" => ");
+                            Console.Write(shortestChain[i + 2]);
+                            i += 2;
+                            continue;
+                        }
+                        break;
+                    }
+                    Console.Write($" or {shortestChain[i + 1]}");
+                    i++;
+                }
+
+                i++;
+            }
+
+            Console.WriteLine('\n');
+        }
+
+        public static void PrintMovieLink()
+        {
+            HashSet<string> movies = new HashSet<string>();
+            int i = 0;
+
+            while (i < movieMap.Count)
+            {
+                List<string> list = movieMap[i];
+                i++;
+                while (i + 1 < movieMap.Count)
+                {
+                    IEnumerable<string> s = movieMap[i].Intersect(movieMap[i + 1]);
+                    foreach (string s2 in s)
+                    {
+                        movies.Add(s2);
+                    }
+                    i++;
+                }
+            }
+
+            Console.Write("MOVIES LINK: ");
+            foreach (string movie in movies)
+            {
+                Console.Write(movie + ' ');
+            }
+            Console.WriteLine('\n');
+        }
+            
+        public static void PrintActorMap()
+        {
+            foreach (KeyValuePair<int, HashSet<int>> pair in actorMap)
+            {
+                Console.Write($"{numberEncoding[pair.Key]} --> ");
+                foreach (int actor in pair.Value)
+                {
+                    Console.Write($"{numberEncoding[actor]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintMovieMap()
+        {
+            foreach (KeyValuePair<int, List<string>> pair in movieMap)
+            {
+                Console.Write($"{numberEncoding[pair.Key]} --> ");
+                foreach (string movie in pair.Value)
+                {
+                    Console.Write($"{movie} / ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+       private static void PrintAllPaths(List<List<int>> paths)
+        {
+            int count = 1;
+            foreach (List<int> path in paths)
+            {
+                int j = 0;
+                Console.Write($"Path {count}: ");
+                while (j < path.Count)
+                {
+                    Console.Write(numberEncoding[path[j]]);
+                    while (j + 1 < path.Count)
+                    {
+                        Console.Write($" -> {numberEncoding[path[j + 1]]}");
+                        j++;
+                    }
+                    j++;
+                }
+                Console.WriteLine();
+                count++;
+            }
+        }
+
+    }
+}
---
+++ SmallWorldPhenomenon_V2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SmallWorldPhenomenon_V2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           var watch = new System.Diagnostics.Stopwatch();
+
+            watch.Start();
+          
+            //Paths to all files are found in the Paths static class
+
+            //Path naming is self-explanatory e.g. MEDIUM_1, LARGE, etc...
+            //If a movie file has two queries then a number is added to the end of query name
+
+            //Just change the following two parameters
+
+            Parser.ReadMovieFile(Paths.SAMPLE);
+            Parser.ReadQueryFile(Paths.SAMPLE_QUERY);
+
+            //Graph.PrintActorMap();
+            //Graph.PrintMovieLink();
+            
+            
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+        }
+    }
+}
